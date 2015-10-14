a80ba4c5c0e87035b30e38996ff2270aea154a3b
Yaron Tal <yaron.tal@oneshoe.nl>
2015-10-14 09:00:27 +0200

    Updated default config file

--- config-example.php
+++ config-example.php
@@ -1,1 +1,6 @@
+<?php
 $config['token'] = 'slack app token';
+$config['welcome_text'] = 'Welcome to our slack channels.'
+. '\n*Please keep passwords and other sensitive information off these channels!*'
+. '\nThese channels are meant for sharing information, interesting links and sending gifs (try /giphy  in the #gif channel ;))!'
+. '\nAlso, to check who has to fetch beer, type \"beer\" in any channel on a friday.';
