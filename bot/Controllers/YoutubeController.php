98c647e7fd731470dbe8825cf6da8aef8b10107c
Yaron Tal <yaron.tal@oneshoe.nl>
2015-11-12 08:52:07 +0100

    Cleaned up guzzle call in youtube controller, using ! before commands and fixed query to youtube

--- bot/Controllers/YoutubeController.php
+++ bot/Controllers/YoutubeController.php
@@ -13,11 +13,18 @@ class YoutubeController {
   public function Youtube(Request $request) {
     $rtm_session = Services::load('API')->getRTMSession();
     $api_key = Services::load('Config')->get('youtube_api_key');
+    $query = explode(' ', $request->getData()->text);
+    array_shift($query); // Remove the !youtube part from the query.
+    $query - implode(' ', $query);
 
     $client = new Client(['base_uri' => 'https://www.googleapis.com/youtube/v3/']);
-    $http_request = $client->get('search?part=id&q='
-      . $request->getData()->text
-      . '&type=video&key=' . $api_key);
+    $http_request = $client->get('search', ['query' => [
+      'part' => 'id',
+      'q' => $query,
+      'type' => 'video',
+      'key' => $api_key,
+    ]]);
+    
     $youtubes = json_decode($http_request->getBody());
 
     $message = 'https://www.youtube.com/watch?v=' . $youtubes->items[0]->id->videoId;
