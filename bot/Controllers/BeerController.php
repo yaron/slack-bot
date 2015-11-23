87cc393396fd8cc51ba082d4e62bc9355f8ba7fd
Yaron Tal <yaron.tal@oneshoe.nl>
2015-11-23 14:01:37 +0100

    Fixed the beer controller to only reply on a friday

--- bot/Controllers/BeerController.php
+++ bot/Controllers/BeerController.php
@@ -14,18 +14,24 @@ class BeerController {
   public function Beer(Request $request) {
     $rtm_session = Services::load('API')->getRTMSession();
 
-    $client = new Client(['base_uri' => $this->base_uri]);
-    $http_request = $client->get($this->json_path);
-    $beer_data = json_decode($http_request->getBody());
+    $day = date('w');
+    if ($day == 5) {
+      $client = new Client(['base_uri' => $this->base_uri]);
+      $http_request = $client->get($this->json_path);
+      $beer_data = json_decode($http_request->getBody());
 
-    $messages = array();
-    $messages[] = 'Staat het bier koud? *' . ($beer_data->cold ? 'Ja!' : 'Nee' ) . '*.';
-    $messages[] = 'Wie moet er koud leggen? ' .  $beer_data->current_bringer . '.';
-    $messages[] = 'Laatst bijgewerkt? ' . $beer_data->last_edit;
-    $messages[] = 'https://www.youtube.com/watch?v=' . $beer_data->youtube;
+      $messages = array();
+      $messages[] = 'Staat het bier koud? *' . ($beer_data->cold ? 'Ja!' : 'Nee') . '*.';
+      $messages[] = 'Wie moet er koud leggen? ' . $beer_data->current_bringer . '.';
+      $messages[] = 'Laatst bijgewerkt? ' . $beer_data->last_edit;
+      $messages[] = 'https://www.youtube.com/watch?v=' . $beer_data->youtube;
 
-    foreach ($messages as $message) {
-      $rtm_session->sendMessage($message, $request->getData()->channel);
+      foreach ($messages as $message) {
+        $rtm_session->sendMessage($message, $request->getData()->channel);
+      }
+    }
+    else {
+      $rtm_session->sendMessage('Is het vrijdag of wat? Mogool.', $request->getData()->channel);
     }
   }
 }
