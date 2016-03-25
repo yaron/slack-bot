d1b46d05149bfb545d14c3906d1d73c81a13787c
Yaron Tal <yaron.tal@oneshoe.nl>
2016-03-25 13:12:50 +0100

    Updated bot to work with the new version of the site

--- bot/Controllers/BeerController.php
+++ bot/Controllers/BeerController.php
@@ -8,22 +8,23 @@ use SlackBot\Services;
 use GuzzleHttp\Client;
 
 class BeerController {
-  protected $base_uri = 'http://www.ligthetbieralkoud.nl';
-  protected $json_path = 'json.php';
-
   public function Beer(Request $request) {
     $rtm_session = Services::load('API')->getRTMSession();
 
     $day = date('w');
     if ($day == 5) {
-      $client = new Client(['base_uri' => $this->base_uri]);
-      $http_request = $client->get($this->json_path);
+      $uri = Services::load('Config')->get('beer_json_uri');
+      $last_slash = strrpos($uri, '/');
+      $path = substr($uri, $last_slash + 1);
+      $uri = substr($uri, 0, $last_slash);
+
+      $client = new Client(['base_uri' => $uri]);
+      $http_request = $client->get($path);
       $beer_data = json_decode($http_request->getBody());
 
       $messages = array();
       $messages[] = 'Staat het bier koud? *' . ($beer_data->cold ? 'Ja!' : 'Nee') . '*.';
       $messages[] = 'Wie moet er koud leggen? ' . $beer_data->current_bringer . '.';
-      $messages[] = 'Laatst bijgewerkt? ' . $beer_data->last_edit;
       $messages[] = 'https://www.youtube.com/watch?v=' . $beer_data->youtube;
 
       foreach ($messages as $message) {
--- config-example.php
+++ config-example.php
@@ -5,3 +5,4 @@ $config['welcome_text'] = 'Welcome to our slack channels.'
 . '\n*Please keep passwords and other sensitive information off these channels!*'
 . '\nThese channels are meant for sharing information, interesting links and sending gifs (try /giphy  in the #gif channel ;))!'
 . '\nAlso, to check who has to fetch beer, type \"beer\" in any channel on a friday.';
+$config['beer_json_uri'] = 'http://www.ligthetbieralkoud.nl/json.php';
